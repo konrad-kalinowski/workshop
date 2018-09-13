a4f0bbd12c566b359d96914caaca7718080e7d38
Konrad Kalinowski <kalinowski.konrad3@gmail.com>
2018-09-13 21:35:39 +0200

    Basic repair form

--- src/main/webapp/app/repair-form/repair-form.component.ts
+++ src/main/webapp/app/repair-form/repair-form.component.ts
@@ -1,21 +1,46 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from 'app/entities/task';
+import { PartService } from 'app/entities/part';
+import { Observable } from 'rxjs';
+import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import { ITask } from 'app/shared/model/task.model';
+import { JhiAlertService } from 'ng-jhipster';
+import { IPart } from 'app/shared/model/part.model';
 
 @Component({
   selector: 'jhi-repair-form',
-  templateUrl: './repair-form.component.html',
-  styleUrls: [
-    'repair-form.scss'
-  ]
+  templateUrl: './repair-form.component.html'
 })
 export class RepairFormComponent implements OnInit {
 
-  message: string;
+  tasks: ITask[];
+  parts: IPart[];
 
-  constructor() {
-    this.message = 'RepairFormComponent message';
+  selectedTasks = [];
+
+  constructor(
+    private jhiAlertService: JhiAlertService,
+    private taskService: TaskService,
+    private partService: PartService) {
+
   }
 
   ngOnInit() {
+    this.taskService.query().subscribe(
+      (res: HttpResponse<ITask[]>) => {
+        this.tasks = res.body;
+      },
+      (res: HttpErrorResponse) => this.onError(res.message)
+    );
+    this.partService.query().subscribe(
+      (res: HttpResponse<IPart[]>) => {
+        this.parts = res.body;
+      },
+      (res: HttpErrorResponse) => this.onError(res.message)
+    );
   }
 
+  private onError(errorMessage: string) {
+    this.jhiAlertService.error(errorMessage, null, null);
+  }
 }
--- src/main/webapp/app/repair-form/repair-form.module.ts
+++ src/main/webapp/app/repair-form/repair-form.module.ts
@@ -1,22 +1,25 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { FormsModule } from '@angular/forms';
 
 import { WorkshopSharedModule } from '../shared';
 
 import { REPAIR_FORM_ROUTE, RepairFormComponent } from './';
 
 @NgModule({
-    imports: [
-      WorkshopSharedModule,
-      RouterModule.forRoot([ REPAIR_FORM_ROUTE ], { useHash: true })
-    ],
-    declarations: [
-      RepairFormComponent,
-    ],
-    entryComponents: [
-    ],
-    providers: [
-    ],
-    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  imports: [
+    WorkshopSharedModule,
+    NgSelectModule, FormsModule,
+    RouterModule.forRoot([REPAIR_FORM_ROUTE], { useHash: true })
+  ],
+  declarations: [
+    RepairFormComponent,
+  ],
+  entryComponents: [
+  ],
+  providers: [
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class WorkshopAppRepairFormModule {}
+export class WorkshopAppRepairFormModule { }
